e46122233209b911944835935dbccab39e09f9df
Jeremy Grifski <jeremy.grifski@therenegadecoder.com>
2018-08-31 21:35:39 -0400

    Cleaned up JUnit test

--- Project01/Project01Test.java
+++ Project01/Project01Test.java
@@ -1,12 +1,25 @@
-// http://web.cse.ohio-state.edu/cse1223/currentsem/projects/CSE1223Project01.html
 import junit.framework.TestCase;
 import java.io.*;
 
+/**
+ * Tests the problem defined by the following URL:
+ * // http://web.cse.ohio-state.edu/cse1223/currentsem/projects/CSE1223Project01.html
+ * 
+ * @author Jeremy Grifski
+ */
 public class Project01Test extends TestCase {
   
   private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
   private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
 
+  /**
+   * Returns a string in "operand operator operand = result" format
+   * 
+   * @param firstVal some integer value
+   * @param secondVal some integer value
+   * @param operator some binary arithmetic operator (+, -, *, /, %)
+   * @return a String in the form "firstVal operator secondVal = result"
+   */
   public static String outputArithmetic(int firstVal, int secondVal, char operator) {
     int result = 0;
     
@@ -32,11 +45,25 @@ public class Project01Test extends TestCase {
     return firstVal + " " + operator + " " + secondVal + " = " + result + System.lineSeparator();
   }
   
+  /**
+   * Returns some string in the form "The average of your two numbers is: average"
+   * 
+   * @param firstVal some integer value
+   * @param secondVal some integer value
+   * @return a string in the form "The average of your two numbers is: average"
+   */
   public static String outputAverage(int firstVal, int secondVal) {
     int average = (firstVal + secondVal) / 2;
     return "The average of your two numbers is: " + average + System.lineSeparator();
   }
   
+  /**
+   * Returns the master string based on the problem defined above
+   * 
+   * @param firstNumber some integer value
+   * @param secondNumber some integer value
+   * @return the master string to be output as the solution
+   */
   public static String generateOutput(int firstNumber, int secondNumber) {
     String output = "Enter the first number: " + System.lineSeparator();
     output += "Enter the second number: " + System.lineSeparator();
@@ -48,6 +75,13 @@ public class Project01Test extends TestCase {
     return output;
   }
 
+  /**
+   * A helper method for running test cases. Pass any two values, and it
+   * generates the proper solution to the problem.
+   * 
+   * @param firstVal some integer value
+   * @param secondVal some integer value
+   */
   public void runCase(int firstVal, int secondVal) {
     System.setOut(new PrintStream(outContent));
     System.setErr(new PrintStream(errContent));
@@ -64,14 +98,23 @@ public class Project01Test extends TestCase {
     System.setOut(System.out);
   }
   
+  /**
+   * The first arbitrary test case
+   */
   public void testMainCase1() {
     runCase(12, 3);
   }
   
+  /**
+   * The second arbitrary test case
+   */
   public void testMainCase2() {
     runCase(5, 7);
   }
   
+  /**
+   * The third arbitrary test case
+   */
   public void testMainCase3() {
     runCase(311, 14);
   }
